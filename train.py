0e551f8525e048fee5a7fd521d333f7db59a4cff
LYkate <linyan5516@163.com>
2025-06-05 19:48:14 +0800

    Update train.py

--- train.py
+++ train.py
@@ -5,27 +5,27 @@ import torch.optim.lr_scheduler as lr_scheduler
 
 data = SummaryWriter()
 
-def train_net(net, device, train_input_root,train_lable_root, epoch=150, batch_size=20, lr=0.0001):
+def train_net(net, device, train_input_root,train_lable_root, batch_size=20, lr=0.0001):
     # 加载训练集
     isbi_dataset_train = MyDataset(train_input_root,train_lable_root)
     train_loader = torch.utils.data.DataLoader(dataset=isbi_dataset_train,
                                                batch_size=batch_size,
                                                shuffle=True,
-                                               num_workers=30,
+                                               num_workers=60,
                                                pin_memory=True)
 
-    # 定义RMSprop算法
-    optimizer = torch.optim.Adam(net.parameters(), lr, weight_decay=1e-4)#权重衰减, weight_decay=1e-6
+    optimizer = torch.optim.Adam(net.parameters(), lr, weight_decay=1e-4)#权重衰减
     # 创建一个学习率调度器
-    scheduler = lr_scheduler.MultiStepLR(optimizer, milestones=[25, 100], gamma=0.1, last_epoch=-1)
+    scheduler = lr_scheduler.MultiStepLR(optimizer, milestones=[30, 70], gamma=0.1, last_epoch=-1)
     # 定义Loss算法
     criterion = nn.MSELoss()
     criterion = criterion.to(device=device)
     step=0
     # best_loss统计，初始化为正无穷
     best_loss = float('inf')
-    # 训练epochs次
-    for epoch in range(epoch):
+    epoch=-1
+    while best_loss > 3e-5:
+        epoch=epoch+1
         # 训练模式
         net.train()
         lr = scheduler.get_last_lr()
@@ -60,7 +60,7 @@ train_lable_root = 'database/label'
 if __name__ == "__main__":
     # 选择设备，有cuda用cuda，没有就用cpu
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    # 加载网络，图片单通道1，分类为255。
+    # 加载网络
     net = PATModel(in_channels=1, out_channels=1)
     # 将网络拷贝到deivce中
     if torch.cuda.device_count() > 1:
