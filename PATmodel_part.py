8cb6f856a224fa5a058b38f7db6df01f7ecc7f00
LYkate <linyan5516@163.com>
2025-06-05 19:24:34 +0800

    Update PATmodel_part.py

--- PATmodel_part.py
+++ PATmodel_part.py
@@ -1,17 +1,58 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from collections import OrderedDict
 
+class SKConv(nn.Module):
+    def __init__(self, in_channels, out_channels, kernels=[1,3,5,7], reduction=16, group=1, L=32):
+        super().__init__()
+        self.out_channels = out_channels
+        self.d = max(in_channels // reduction, L)
+        self.convs = nn.ModuleList([])
+        for i in kernels:
+            self.convs.append(
+                nn.Sequential(OrderedDict([
+                    ('conv',nn.Conv2d(in_channels, out_channels, kernel_size=i, padding=i // 2, groups=group)),
+                    ('bn',nn.BatchNorm2d(out_channels)),
+                    ('relu',nn.ReLU(inplace=True))
+                ]))
+            )
+        self.fc = nn.Linear(out_channels, self.d)
+        self.fcs = nn.ModuleList([])
+        for i in range(len(kernels)):
+            self.fcs.append(
+                nn.Linear(self.d, out_channels)
+            )
+        self.softmax = nn.Softmax(dim=0)
+
+    def forward(self, x):
+        batch_size, c, _, _ = x.size()
+        conv_outs = []
+        # 1.split
+        for conv in self.convs:
+            conv_outs.append(conv(x))
+        feats = torch.stack(conv_outs, 0)
+        # 2.fuse
+        feats_U = sum(conv_outs)
+        feats_S = feats_U.mean(-1).mean(-1)
+        feats_Z = self.fc(feats_S)
+        # 3.select
+        weights = []
+        for fc in self.fcs:
+            weight = fc(feats_Z)
+            weights.append(weight.view(batch_size, self.out_channels, 1, 1))
+        attention_weights = torch.stack(weights, 0)
+        attention_weights = self.softmax(attention_weights)
+        # 4.fuse
+        out=(attention_weights*feats).sum(0)
+        return out
+        
 class DoubleConv(nn.Module):
     def __init__(self, in_channels, out_channels):
         super().__init__()
         self.double_conv = nn.Sequential(
-            nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
-            nn.BatchNorm2d(out_channels),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1),
-            nn.BatchNorm2d(out_channels),
-            nn.ReLU(inplace=True)
+            SKConv(in_channels, out_channels),
+            SKConv(out_channels, out_channels),
         )
 
     def forward(self, x):
