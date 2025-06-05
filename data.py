983ddca7b81711dfee7ad7840b7fbcf44cbecb82
LYkate <linyan5516@163.com>
2025-06-05 20:13:40 +0800

    Update data.py

--- data.py
+++ data.py
@@ -10,12 +10,9 @@ from matplotlib import pyplot as plt
 import torch.nn.functional as F
 import cv2
 
-#transforms_imag=torchvision.transforms.Compose([torchvision.transforms.Resize[572,572],
-#                                                torchvision.transforms.ToTensor(),])
-
 class MyDataset(Dataset): #继承了Dataset类
     def __init__(self,input_root,lable_root):
-        #分别读取输入/标签图片的路径信息
+        #分别读取输入/标签的路径信息
         self.input_root=input_root
         self.input_files=os.listdir(input_root)
 
@@ -33,11 +30,9 @@ class MyDataset(Dataset): #继承了Dataset类
     def __getitem__(self, index):
        input_signal_path=os.path.join(self.input_root,self.input_files[index])
        input_ex=pd.read_excel(input_signal_path, header=None)
-       #input_img=F.rgb2gray(input_img)
 
        lable_img_path = os.path.join(self.lable_root, self.lable_files[index])
        lable_img = Image.open(lable_img_path)
-       #lable_img = F.rgb2gray(lable_img)
 
        input_signal=torch.tensor(input_ex.values)
        input_signal=torch.unsqueeze(input_signal, 0)
